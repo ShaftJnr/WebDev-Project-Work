5b73b3a1391609d26821127cdcb7775f996d7d18
Jeho Acquah <draco.code.4l@gmail.com>
2023-04-19 22:09:17 +0000

    updates
    updates to the HTML and CSS of the sign up page.

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,14 @@
+//login function
 function showAlert() {
   alert("You have successfully logged-in!");
 }
 
+//contact-us function
 function showAlert() {
   alert("Your message was sent successfully!");
+}
+
+//sign-up function
+function showAlert() {
+  alert("Sign-up Successful!");
 }
